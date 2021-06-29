676a6b7fc0b7909af238709432b7721f1f1379c9
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-06-29 06:30:03 +0200

    Steps added

--- components/Steps.vue
+++ components/Steps.vue
@@ -62,7 +62,20 @@
                       rounded-full
                     "
                   >
-                    X
+                    <svg
+                      class="w-6 h-6"
+                      fill="none"
+                      stroke="white"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M5 13l4 4L19 7"
+                      ></path>
+                    </svg>
                   </span>
                 </span>
                 <span class="mt-0.5 ml-4 min-w-0 flex flex-col">
